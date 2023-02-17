bec79ecf7ebf7157f2d2a627b66922dfd87c26e7
JATINKUMAR1008 <97216852+JATINKUMAR1008@users.noreply.github.com>
2023-02-17 22:03:41 +0530

    navbar done

--- Frontend/client-ui/src/components/Navbar/Current_Nav/header.jsx
+++ Frontend/client-ui/src/components/Navbar/Current_Nav/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import book_svg from "../../../assets/undraw_notebook_re_id0r.svg"
 import "./header.css"
 const menu_items = ["Home","Blog","Subscription","About Us","Publication"]
 const Header = () => {
   return (
     <div className='header-container'>
         <div className='Title'>
-            B00k5..
+            Rise
         </div>
         <div className="listwrapper">
             <div className='navList'>
