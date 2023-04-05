ba1615418a7337ff8b8dd962c64b5e7c19a9e711
Jatin Saini <jatinkhushi5555@gmail.com>
2023-04-05 21:46:03 +0530

    Product Details page added

--- Frontend/client-ui/src/App.js
+++ Frontend/client-ui/src/App.js
@@ -1,41 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/toggle_Navbar/Navbar';
+// import Navbar from './components/Navbar/toggle_Navbar/Navbar';
 import Header from './components/Navbar/Current_Nav/header';
 import Hero from './components/Heropage/Hero';
 import LoginForm from './components/Login/Signup/login';
 import SignupForm from './components/Login/Signup/signup';
 import Nav from './components/Navbar/New_nav/nav';
-import { Route,Routes,Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Footer from './components/Footer/Footer';
 import Product from './components/Product_modal/Product';
-import E_books from './components/E-books';
+import Ebooks from './components/E-books/index';
 import SubCards from './components/Subscriptions';
 import Publication from './components/Publication';
 import Account from './components/User/Account';
 function App() {
- const[login,setLogin]=useState(true)
- const handleLogin=()=>{
-  setLogin(!login)
- }
+  const [login, setLogin] = useState(false)
+  const handleLogin = () => {
+    setLogin(!login)
+  }
   return (
     <div className="App">
-      <Nav login={login}/>
-      <Header login={login} handleLogin={handleLogin}/>
+      <Nav login={login} />
+      <Header login={login} handleLogin={handleLogin} />
       <Routes>
-        <Route path='/' element={<Hero/>}/>
-        <Route path='/login' element={<LoginForm/>}/>
-        <Route path="/signup" element={<SignupForm/>}/>
-        <Route path="/book/:id" element={<Product/>}/>
-        <Route path="/ebook" element={<E_books/>}/>
-        <Route path="/sub" element={<SubCards/>}/>
-        <Route path="/publications" element={<Publication/>}/>
-        <Route path="/Account/:id" element={<Account/>}/>
+        <Route path='/' element={<Hero />} />
+        <Route path='/login' element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/book/:id" element={<Product />} />
+        <Route path="/ebook" element={<Ebooks />} />
+        <Route path="/sub" element={<SubCards />} />
+        <Route path="/publications" element={<Publication />} />
+        <Route path="/Account/:id" element={<Account />} />
       </Routes>
-      <Footer/>
-      
-      
+      <Footer />
+
+
     </div>
   );
 }
